b68133c07f050c7ef3ccd0a415bc7c74f1daf335
Rodrigo Fuentealba <rodrigofuentealba44@gmail.com>
2025-12-23 12:04:18 -0300

    Fixes de rendimiento

---
+++ config/calendar-config.js
@@ -0,0 +1,184 @@
+const icsUrls = {
+    "FHGCP DECANATO AUDITORIO 👤126":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0ad27548c1c4d17bd6de44bdcbaac51707574365493584925/calendar.ics",
+    "FHGCP DECANATO SALA CONSEJO FACULTAD 👤25":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/4cba199c6466429dbe5eeccacc4ee4cc1280787131942128037/calendar.ics",
+    "FHGCP SALA CONSEJO CIENCIA POLITICA 👤12":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/9ebb8e393e3c4b7ea826cec235e28fcc4163901858859466902/calendar.ics",
+    "FHGCP SALA CONSEJO GEOGRAFIA 👤8":
+        "http://outlook.office365.com/owa/calendar/4b0b3a0a1fc6489b87524b9b7ed9c687@uc.cl/8d66e4c06f5e4b4692af362963a98da03628685577008031525/calendar.ics",
+    "FHGCP SALA CONSEJO HISTORIA 👤12":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/8f820fe3c7f44ed89a4cba45e25059cf5916755005309354082/calendar.ics",
+    "FHGCP SALA SIMULACION ICP 1 👤25":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/d8ad107659ff45688d71986a1ff1bdea4191855137671217506/calendar.ics",
+    "FHGCP SALA SIMULACION ICP 2 👤35":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6d8e954e24db4a99a848d49a714497e913370319527010283519/calendar.ics",
+    "FHGCP SALA DOCTORADO CIENCIA POLITICA 👤12":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/5c88f558df38410f9daea0d77aa71d529970456336550474037/calendar.ics",
+    "FHGCP SALA DOCTORADO GEOGRAFIA 👤10":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/f0224482db574219ad94fe9ae52c6ca04585519317557358076/calendar.ics",
+    "FHGCP_PASILLO_2DO_PISO_👤40":
+        "http://outlook.office365.com/owa/calendar/24e8cbb9d1424f5d81f3c5d20f9953fd@uc.cl/5c8857a5528e4661aa906d6bc180eb4a6372096918242644660/calendar.ics",
+    "FHGCP SALA POSTGRADO HISTORIA 👤10":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/c3b48916586640eab1e50da622d099a418067958400765017998/calendar.ics",
+    "FHGCP SALA REUNIONES A 7 👤10":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/120d13ec043e490b8165867b68b27e9118025989541280493575/calendar.ics",
+    "FHGCP SALA USOS MULTIPLES 👤22":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6c91932a0de541c3b05a8ef1c4764ba314100958707069186750/calendar.ics",
+    "FHGCP SEMINARIO 1 👤20":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/06563fa029cf45eabbc316217c589c6710661106583453533547/calendar.ics",
+    "FHGCP SEMINARIO 3 👤20":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/60cd401e6d924d04bc193e4bd51c99843051329826593012105/calendar.ics",
+    "FHGCP SEMINARIO 4 👤14":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0838498c72642b5ba19f5b642e9864b4211631172097908883/calendar.ics",
+    "FHGCP SEMINARIO 5 👤14":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6802f81d14634476991b7a187d2a9d212612002183572204460/calendar.ics",
+    "FHGCP SEMINARIO 6 👤12":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/3c06d828811349978116158df637d26f16742055330210283400/calendar.ics",
+    "FHGCP SEMINARIO FACULTAD 👤41":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0b8f33c6b0944f8b0d2c309d20e7037812544073743176903/calendar.ics",
+    "FHGCP TERRAZA 👤50":
+        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/f3008c0a5f944aa8b35933dae17810dc9850426923127246455/calendar.ics",
+};
+
+const reservationConfig = {
+    "FHGCP_SALA_REUNIONES_A_7_👤10": {
+        email: "FHGCP-A-7@uc.cl",
+        location: "FHGCP-A-7@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_DECANATO_AUDITORIO_👤126": {
+        email: "FHGCP-DECANATO-AUDITORIO@uc.cl",
+        location: "FHGCP-DECANATO-AUDITORIO@uc.cl",
+        defaultDurationMinutes: 60,
+        body: "Seleccione SOLO el equipamiento que necesita para la actividad."
+    },
+
+    "FHGCP_DECANATO_SALA_CONSEJO_FACULTAD_👤25": {
+        email: "FHGCP-DECANATO-SALA-CONSEJO-FACULTAD@uc.cl",
+        location: "FHGCP-DECANATO-SALA-CONSEJO-FACULTAD@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_DOCTORADO_GEOGRAFIA_👤10": {
+        email: "FHGCP-DOCTORADO-GEOGRAFIA@uc.cl",
+        location: "FHGCP-DOCTORADO-GEOGRAFIA@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_DOCTORADO_CIENCIA_POLITICA_👤12": {
+        email: "FHGCP-DOCTORADO-ICP@uc.cl",
+        location: "FHGCP-DOCTORADO-ICP@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_DOCTORADO_ICP_2": {
+        email: "FHGCP-DOCTORADO-ICP-2@uc.cl",
+        location: "FHGCP-DOCTORADO-ICP-2@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_PASILLO_2DO_PISO_👤40": {
+        email: "FHGCP-PASILLO-2DO-PISO-FACULTAD@uc.cl",
+        location: "FHGCP-PASILLO-2DO-PISO-FACULTAD@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_LABORATORIO_DE_GEOMATICA_👤14": {
+        email: "FHGCP-LABORATORIO-DE-GEOMATICA@uc.cl",
+        location: "FHGCP-LABORATORIO-DE-GEOMATICA@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_CONSEJO_HISTORIA_👤12": {
+        email: "FHGCP-SALA-CONSEJO-HISTORIA@uc.cl",
+        location: "FHGCP-SALA-CONSEJO-HISTORIA@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_CONSEJO_CIENCIA_POLITICA_👤12": {
+        email: "FHGCP-SALA-CONSEJO-ICP@uc.cl",
+        location: "FHGCP-SALA-CONSEJO-ICP@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_CONSEJO_GEOGRAFIA_👤8": {
+        email: "FHGCP-SALA-DE-CONSEJO-INSTITUTO-DE-GEOGRAFIA@uc.cl",
+        location: "FHGCP-SALA-DE-CONSEJO-INSTITUTO-DE-GEOGRAFIA@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_POSTGRADO_HISTORIA_👤10": {
+        email: "FHGCP-SALA-SEMINARIO-HISTORIA@uc.cl",
+        location: "FHGCP-SALA-SEMINARIO-HISTORIA@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SALA_USOS_MULTIPLES_👤22": {
+        email: "FHGCP-SALA-USOS-MULTIPLES@uc.cl",
+        location: "FHGCP-SALA-USOS-MULTIPLES@uc.cl",
+        defaultDurationMinutes: 60,
+        body: "Seleccione SOLO el equipamiento que necesita para la actividad."
+    },
+
+    "FHGCP_SEMINARIO_1_👤20": {
+        email: "FHGCP-SEMINARIO1@uc.cl",
+        location: "FHGCP-SEMINARIO1@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SEMINARIO_3_👤20": {
+        email: "FHGCP-SEMINARIO-3@uc.cl",
+        location: "FHGCP-SEMINARIO-3@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SEMINARIO_4_👤14": {
+        email: "FHGCP-SEMINARIO-4@uc.cl",
+        location: "FHGCP-SEMINARIO-4@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SEMINARIO_5_👤14": {
+        email: "FHGCP-SEMINARIO-5@uc.cl",
+        location: "FHGCP-SEMINARIO-5@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SEMINARIO_6_👤12": {
+        email: "FHGCP-SEMINARIO-6@uc.cl",
+        location: "FHGCP-SEMINARIO-6@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SEMINARIO_FACULTAD_👤41": {
+        email: "FHGCP-SEMINARIO-FACULTAD@uc.cl",
+        location: "FHGCP-SEMINARIO-FACULTAD@uc.cl",
+        defaultDurationMinutes: 60,
+        body: "Seleccione SOLO el equipamiento que necesita para la actividad."
+    },
+
+    "FHGCP_SIMULACION_ICP_1_👤25": {
+        email: "FHGCP-SIMULACION-ICP@uc.cl",
+        location: "FHGCP-SIMULACION-ICP@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_SIMULACION_ICP_2_👤35": {
+        email: "FHGCP-SIMULACION-ICP2@uc.cl",
+        location: "FHGCP-SIMULACION-ICP2@uc.cl",
+        defaultDurationMinutes: 60
+    },
+
+    "FHGCP_TERRAZA_👤50": {
+        email: "FHGCP-TERRAZA@uc.cl",
+        location: "FHGCP-TERRAZA@uc.cl",
+        defaultDurationMinutes: 60
+    }
+};
+
+module.exports = {
+    icsUrls,
+    reservationConfig
+};
--- server.js
+++ server.js
@@ -3,54 +3,10 @@ const path = require("path");
 const axios = require("axios");
 const morgan = require("morgan"); // Logging middleware
 const { RRule, rrulestr } = require("rrule"); // Importar RRule
+const { icsUrls } = require("./config/calendar-config");
 
 const app = express();
 const port = 3000;
-
-// Define your ICS URLs mapping
-    const icsUrls = {
-      "FHGCP DECANATO AUDITORIO 👤126":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0ad27548c1c4d17bd6de44bdcbaac51707574365493584925/calendar.ics",
-      "FHGCP DECANATO SALA CONSEJO FACULTAD 👤25":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/4cba199c6466429dbe5eeccacc4ee4cc1280787131942128037/calendar.ics",
-      "FHGCP SALA CONSEJO CIENCIA POLITICA 👤12":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/9ebb8e393e3c4b7ea826cec235e28fcc4163901858859466902/calendar.ics",
-      "FHGCP SALA CONSEJO GEOGRAFIA 👤8":
-        "http://outlook.office365.com/owa/calendar/4b0b3a0a1fc6489b87524b9b7ed9c687@uc.cl/8d66e4c06f5e4b4692af362963a98da03628685577008031525/calendar.ics",
-      "FHGCP SALA CONSEJO HISTORIA 👤12":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/8f820fe3c7f44ed89a4cba45e25059cf5916755005309354082/calendar.ics",
-      "FHGCP SALA SIMULACION ICP 1 👤25":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/d8ad107659ff45688d71986a1ff1bdea4191855137671217506/calendar.ics",
-      "FHGCP SALA SIMULACION ICP 2 👤35":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6d8e954e24db4a99a848d49a714497e913370319527010283519/calendar.ics",
-      "FHGCP SALA DOCTORADO CIENCIA POLITICA 👤12":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/5c88f558df38410f9daea0d77aa71d529970456336550474037/calendar.ics",
-      "FHGCP SALA DOCTORADO GEOGRAFIA 👤10":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/f0224482db574219ad94fe9ae52c6ca04585519317557358076/calendar.ics",
-      "FHGCP_PASILLO_2DO_PISO_👤40":
-        "http://outlook.office365.com/owa/calendar/24e8cbb9d1424f5d81f3c5d20f9953fd@uc.cl/5c8857a5528e4661aa906d6bc180eb4a6372096918242644660/calendar.ics",
-      "FHGCP SALA POSTGRADO HISTORIA 👤10":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/c3b48916586640eab1e50da622d099a418067958400765017998/calendar.ics",
-      "FHGCP SALA REUNIONES A 7 👤10":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/120d13ec043e490b8165867b68b27e9118025989541280493575/calendar.ics",
-      "FHGCP SALA USOS MULTIPLES 👤22":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6c91932a0de541c3b05a8ef1c4764ba314100958707069186750/calendar.ics",
-      "FHGCP SEMINARIO 1 👤20":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/06563fa029cf45eabbc316217c589c6710661106583453533547/calendar.ics",
-      "FHGCP SEMINARIO 3 👤20":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/60cd401e6d924d04bc193e4bd51c99843051329826593012105/calendar.ics",
-      "FHGCP SEMINARIO 4 👤14":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0838498c72642b5ba19f5b642e9864b4211631172097908883/calendar.ics",
-      "FHGCP SEMINARIO 5 👤14":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/6802f81d14634476991b7a187d2a9d212612002183572204460/calendar.ics",
-      "FHGCP SEMINARIO 6 👤12":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/3c06d828811349978116158df637d26f16742055330210283400/calendar.ics",
-      "FHGCP SEMINARIO FACULTAD 👤41":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/a0b8f33c6b0944f8b0d2c309d20e7037812544073743176903/calendar.ics",
-      "FHGCP TERRAZA 👤50":
-        "https://outlook.office365.com/owa/calendar/1b3e56414b82479fa4b82073497d1e38@uc.cl/f3008c0a5f944aa8b35933dae17810dc9850426923127246455/calendar.ics",
-    };
-
 
 app.use(morgan("combined")); // Use morgan for logging
 app.use(express.static(path.join(__dirname, "public")));
